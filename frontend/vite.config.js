f840d9abe799aa7bf4e4bd68a091f02ecdd44066
Ephraim Debel <ephraimdebel@gmail.com>
2025-04-20 15:43:50 +0300

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
   server: {
-    sourcemap: false,
+    // sourcemap: false,
+    historyApiFallback: true
   },
 })
 
